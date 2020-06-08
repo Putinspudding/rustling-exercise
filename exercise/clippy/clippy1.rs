e291505c10e8c8db69fa396d7d527ef71c6f7056
Pwdding <sweetss90@126.com>
2020-06-08 22:14:55 +0800

    clippy finished

---
+++ exercise/clippy/clippy1.rs
@@ -0,0 +1,16 @@
+// clippy1.rs
+// The Clippy tool is a collection of lints to analyze your code
+// so you can catch common mistakes and improve your Rust code.
+//
+// Execute `rustlings hint clippy1` for hints :)
+
+
+
+fn main() {
+    let x = 1.2331f64;
+    let y = 1.2332f64;
+    let error = 0f64;
+    if (y - x).abs() > error {
+        println!("Success!");
+    }
+}
---
+++ exercise/clippy/target/debug/deps/clippy2-db718e1bf7bd8e75.d
@@ -0,0 +1,5 @@
+/root/rustlings/rustlings/exercises/clippy/target/debug/deps/clippy2-db718e1bf7bd8e75.rmeta: clippy2.rs
+
+/root/rustlings/rustlings/exercises/clippy/target/debug/deps/clippy2-db718e1bf7bd8e75.d: clippy2.rs
+
+clippy2.rs:
